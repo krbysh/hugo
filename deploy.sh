2b2f700772b544fab74900a1e3879673c1a3a989
krbysh <y.krbysh@gmail.com>
2019-11-13 02:52:41 +0900

    source deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,5 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 sleep 5
-git subtree pull --prefix=public https://github.com/krbysh/hugo.git gh-pages
-git subtree push --prefix=public https://github.com/krbysh/hugo.git gh-pages
+git push https://github.com/krbysh/hugo.git `git subtree split --prefix=public --onto=https://github.com/krbysh/hugo.git/gh-pages`:gh-pages
+# git subtree push --prefix=public https://github.com/krbysh/hugo.git gh-pages
